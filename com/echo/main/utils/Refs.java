341c90517e28f37d63d09fb5bf9f16b5c49175e6
NOcturnais <NOcturnais1412@gmail.com>
2020-05-27 15:08:21 +0200

    Packages + utils

---
+++ com/echo/main/utils/Refs.java
@@ -0,0 +1,6 @@
+public static final String MODID = "echoglory";
+public static final String VERSION = "1.0.00";
+public static final String COMMON_PROXY = "com.echo.main.common.CommonProxy";
+public static final boolean = null;
+public static final String CLIENT_PROXY = "com.echo.main.client.ClientProxy";
+public static final String NAME = "Echo Glory"
